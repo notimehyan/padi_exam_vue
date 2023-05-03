59d0fbf66003c92df27213cbfa8672f145480b17
rock <notime.hy@gmail.com>
2023-05-03 22:37:57 +0800

    23123123

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,25 @@
 <template>
   <div>
-    <button v-on:click="isShow = !isShow">显示/隐藏</button>
-    <h3 v-show="isShow">show盒子</h3>
-    <h3 v-if="isShow">if盒子</h3>
+    <h3 v-if="flag">vip 来了</h3>
+    <h3 v-else>先登录</h3>
+    <hr>
+    <h3 v-if="age > 10">1-10 dasda  </h3>
+    <h3 v-else-if="age >=20">11-20 dasda</h3>
+    <h3 v-else>21-30 </h3>
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      isShow: true,
-    };
-  },
-  methods: {
-    
-  },
-};
+  data(){
+    return{
+      flag: true,
+      age:27
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+
+</style>
---
+++ src/App06-shouw_if.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button v-on:click="isShow = !isShow">显示/隐藏</button>
+    <h3 v-show="isShow">show盒子</h3>
+    <h3 v-if="isShow">if盒子</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShow: true,
+    };
+  },
+  methods: {
+    
+  },
+};
+</script>
+
+<style></style>
