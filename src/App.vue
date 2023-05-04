b311bf2dc0ce80bdb6c23b9ee364f7323e4b2822
A10A-ROCK-F383-\admin <jsrock@it888.com>
2023-05-04 17:37:28 +0800

    121212

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,10 @@
 <template>
-  <div id="app">
-    <h3>案例：折叠面板</h3>
-    <div class="box">
-      <div class="title">
-        <h4>芙蓉楼送辛渐</h4>
-        <!-- 按钮位置 -->
-        <span class="btn" @click="isShow = !isShow">
-          {{ isShow ? '收起' : '展开' }}
-        </span>
-      </div>
+  <div>
+    <ul>
+      <li v-for="item in arr" :key="item">{{ item }}</li>
+    </ul>
 
-      <!-- 切换显示隐藏的部分 (数据驱动) -->
-      <div v-show="isShow" class="container">
-        <p>寒雨连江夜入吴,</p>
-        <p>平明送客楚山孤。</p>
-        <p>洛阳亲友如相问，</p>
-        <p>一片冰心在玉壶。</p>
-      </div>
-    </div>
+    <button @click="addNew">新来的</button>
   </div>
 </template>
 
@@ -25,54 +12,15 @@
 export default {
   data() {
     return {
-      isShow: true
-    }
-  }
-}
+      arr: ["老大", "老二", "老三","老四"],
+    };
+  },
+  methods: {
+    addNew() {
+      this.arr.splice(1,0,'新人')
+    },
+  },
+};
 </script>
 
-<style lang="less">
-body {
-  background-color: #ccc;
-  #app {
-    width: 400px;
-    height: 320px;
-    margin: 20px auto;
-    background-color: #fff;
-    border: 4px solid rgb(226, 153, 43);
-    border-radius: 1em;
-    box-shadow: 3px 3px 3px rgba(0, 0, 0, 0.5);
-    padding: 1em 2em 2em;
-    h3 {
-      text-align: center;
-    }
-    .box {
-      box-shadow: 0 1px 4px 0 rgba(0, 0, 0, 0.5);
-      border-radius: 4px;
-      border: 1px solid #ebeef5;
-      background-color: #fff;
-      overflow: hidden;
-      color: #303133;
-      transition: .3s;
-    }
-    .title {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      padding: 10px 20px;
-    }
-    .title h4 {
-      line-height: 2;
-      margin: 0;
-    }
-    .container {
-      padding: 0 20px;
-      border-top: 1px solid #ccc;
-    }
-    .btn {
-      /* 鼠标改成手的形状 */
-      cursor: pointer;
-    }
-  }
-}
-</style>
+<style></style>
---
+++ src/App12-for.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="item in arrList" :key="item">{{ item }}</li>
+    </ul>
+
+    <hr />
+
+    <ul>
+      <li v-for="(item, index) in arrList" :key="item">
+        {{ item }} - {{ index }}
+      </li>
+    </ul>
+
+    <br />
+
+    <ul>
+      <li v-for="(value, key) in exam" :key="value">{{ value }}/{{ key }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      arrList: ["12", "23", "34", "45"],
+      exam: {
+        tittle: "2. 下潜时,我需要平衡压力.以下哪些是平衡耳朵的正确技巧?",
+        answerA: "A 捏着鼻子,然后试着从鼻子轻轻吹气.",
+        answerB: "B 做吞咽的动作,左右活动下巴.",
+        answerC: "C 捏着鼻子,然后用力地长时间从鼻子吹气.",
+      },
+    };
+  },
+};
+</script>
+
+<style></style>
