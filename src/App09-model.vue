8dd2752134a7a1ff03ae7b09ef56131e700029a5
A10A-ROCK-F383-\admin <jsrock@it888.com>
2023-05-04 13:58:26 +0800

    2323232

---
+++ src/App09-model.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    用户名<input type="text" v-model="userName" /> 
+    密码<input type="password" v-model="userPassword"/>
+    <button @click="login">登录</button>
+    <button @click="reset">重置</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      userName: "",
+      userPassword: "",
+    };
+  },
+  methods: {
+    login() {
+      console.log(this.userName, this.userPassword);
+    },
+    reset() {},
+  },
+};
+</script>
+
+<style>
+</style>F
---
+++ src/App09-model02.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <select v-model="cityID">
+      <option value="101">beijing</option>
+      <option value="102">shanghai</option>
+      <option value="103">guangdong</option>
+      <option value="104">chengdu</option>
+      <option value="105">chongqing</option>
+    </select>
+    <div>{{ cityID }}</div>
+
+    <input type="text" v-model.number="age">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cityID: " ",
+      age:''
+    };
+  },
+};
+</script>
+
+<style></style>
