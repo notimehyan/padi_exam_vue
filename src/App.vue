eac299e0a3df6d0b662b85d3052e2a63f2ae49b7
A10A-ROCK-F383-\admin <jsrock@it888.com>
2023-05-05 17:24:41 +0800

    23232

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
   <div>
-    <ul>
-      <li v-for="item in arr" :key="item">{{ item }}</li>
-    </ul>
-
-    <button @click="addNew">新来的</button>
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
   </div>
 </template>
 
 <script>
+
 export default {
-  data() {
-    return {
-      arr: ["老大", "老二", "老三","老四"],
-    };
-  },
-  methods: {
-    addNew() {
-      this.arr.splice(1,0,'新人')
-    },
-  },
-};
+
+}
 </script>
 
-<style></style>
+<style>
+
+</style>
